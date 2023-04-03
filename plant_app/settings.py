083db83d82415bcff1e02b13b3b2f5bafc167595
AlexMiller93 <alexandr.miller93@gmail.com>
2023-04-03 12:06:24 +0300

    Add password reset functionality

--- plant_app/settings.py
+++ plant_app/settings.py
@@ -144,3 +144,6 @@ except ImportError:
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
