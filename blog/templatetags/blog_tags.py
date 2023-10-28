294bab9fe6baf8d360eba79db6e5c91a090187ec
AlexMiller93 <alexandr.miller93@gmail.com>
2023-10-28 13:08:23 +0300

    add custom filter in blog app

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,13 +1,16 @@
+
+import markdown
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
 
 from ..models import Post
 
 register = template.Library()
 
 
-@register.inclusion_tag('blog/posts/latest_posts.html')
-def show_latest_posts(pk=None, count=5):
+@register.simple_tag()
+def show_latest_posts(pk=None, count=2):
     if pk:
         latest_posts = Post.objects.filter(author__id=pk).order_by('-created_on')[:count]
     else:
@@ -44,4 +47,8 @@ def get_most_visited_posts(pk=None, count=5):
         return Post.objects.annotate(
             total_likes=Count('seen_by')).order_by('-created_on')[:count]
 
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
 
